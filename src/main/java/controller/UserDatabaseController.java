f3a06daf94578eafd17a63df7956aec06d059c0a
mohammad reza <mz2012kh82@gmail.com>
2022-04-28 13:09:02 +0430

    use class user in UserDatabaseController

--- src/main/java/controller/UserDatabaseController.java
+++ src/main/java/controller/UserDatabaseController.java
@@ -1,6 +1,7 @@
 package controller;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import model.User;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,6 +12,7 @@ import java.util.HashMap;
 import java.util.List;
 
 public class UserDatabaseController {
+
 
     public void initialize() { // for first use
         ArrayList<HashMap<String, String>> users = new ArrayList<>();
@@ -60,25 +62,29 @@ public class UserDatabaseController {
         return -1;
     }
 
-    public void addUser(HashMap<String, String> newUser) { // add new user to database
+    public void addUser(User newUser) { // add new user to database
         ArrayList<HashMap<String, String>> users = this.loadDatabase();
-        if (this.getUserIndexByUsername(newUser.get("username")) != -1) {
-            System.out.println("user with username " + newUser.get("username") + " already exists");
-        } else if (this.getUserIndexByNickname(newUser.get("nickname")) != -1) {
-            System.out.println("user with nickname " + newUser.get("nickname") + " already exists");
+        if (this.getUserIndexByUsername(newUser.getUsername()) != -1) {
+            System.out.println("user with username " + newUser.getUsername() + " already exists");
+        } else if (this.getUserIndexByNickname(newUser.getNickname()) != -1) {
+            System.out.println("user with nickname " + newUser.getNickname() + " already exists");
         } else {
-            users.add(newUser);
+            HashMap<String, String> userNew = new HashMap<>();
+            userNew.put("username", newUser.getUsername());
+            userNew.put("password", newUser.getPassword());
+            userNew.put("nickname", newUser.getNickname());
+            users.add(userNew);
             this.updateDatabase(users);
         }
     }
 
-    public void changeNickname(String username, HashMap<String, String> command) { // this method needs player username
+    public void changeNickname(User user, HashMap<String, String> command) { // this method needs player username
         String newNickname = command.get("nickname");
         ArrayList<HashMap<String, String>> users = this.loadDatabase();
         int index;
         if ((index = getUserIndexByNickname(newNickname)) != -1) {
             System.out.println("user with nickname " + newNickname + " already exists");
-        } else if ((index = getUserIndexByUsername(username)) == -1) {
+        } else if ((index = getUserIndexByUsername(user.getUsername())) == -1) {
             System.out.println("wrong username");
         } else {
             users.get(index).put("nickname", newNickname);
@@ -87,11 +93,11 @@ public class UserDatabaseController {
         this.updateDatabase(users);
     }
 
-    public void changePassword(String username, HashMap<String, String> command) { // this method needs player username
+    public void changePassword(User user, HashMap<String, String> command) { // this method needs player username
         String oldPassword = command.get("old-password");
         String newPassword = command.get("new-password");
         ArrayList<HashMap<String, String>> users = this.loadDatabase();
-        int index = getUserIndexByUsername(username);
+        int index = getUserIndexByUsername(user.getUsername());
         if (index == -1) {
             System.out.println("wrong username");
         } else if (users.get(index).get("password").compareTo(oldPassword) != 0) {
