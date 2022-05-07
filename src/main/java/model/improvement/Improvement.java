302554ff02c853ef73d49b26b3e1eb0e2bd89a53
Sam Khanaki <sam.khanakiskh@gmail.com>
2022-05-07 11:53:32 +0430

    completed improvements

--- src/main/java/model/improvement/Improvement.java
+++ src/main/java/model/improvement/Improvement.java
@@ -11,58 +11,59 @@ import java.util.ArrayList;
 import java.util.List;
 
 public enum Improvement{
-    CAMP(0, 0, 0, Research.TRAPPING,
+    CAMP(0, 0, 0, 6, Research.TRAPPING,
             List.of(TUNDRA, FIELD, HILL),
             List.of(FOREST)
     ),
-    FARM(1, 0, 0, Research.AGRICULTURE,
+    FARM(1, 0, 0, 6, Research.AGRICULTURE,
             List.of(FIELD, GRASS, DESERT),
             List.of()
     ),
-    LUMBER_MILL(0, 0, 1, Research.CONSTRUCTION,
+    LUMBER_MILL(0, 0, 1, 6, Research.CONSTRUCTION,
             List.of(),
             List.of(FOREST)
     ),
-    MINE(0, 0, 1, Research.MINING,
+    MINE(0, 0, 1, 6, Research.MINING,
             List.of(DESERT, FIELD, GRASS, TUNDRA, SNOW, HILL),
             List.of(FOREST, JUNGLE, MARSH)
     ),
-    PASTURE(0, 0, 0, Research.ANIMAL_HUSBANDRY,
+    PASTURE(0, 0, 0, 7, Research.ANIMAL_HUSBANDRY,
             List.of(FIELD, DESERT, GRASS, TUNDRA, HILL),
             List.of()
     ),
-    PLANTATION(0, 0, 0, Research.CALENDAR,
+    PLANTATION(0, 0, 0, 5, Research.CALENDAR,
             List.of(FIELD, DESERT, GRASS),
             List.of(FOREST, JUNGLE, MARSH, PLAIN)
     ),
-    QUARRY(0, 0, 0, Research.MASONRY,
+    QUARRY(0, 0, 0, 7, Research.MASONRY,
             List.of(FIELD, DESERT, GRASS, TUNDRA, HILL),
             List.of()
     ),
-    TRADING_POST(0, 1, 0, Research.TRAPPING,
+    TRADING_POST(0, 1, 0, 8, Research.TRAPPING,
             List.of(FIELD, DESERT, GRASS, TUNDRA),
             List.of()
     ),
-    FACTORY(0, 0, 2, Research.ENGINEERING,
+    FACTORY(0, 0, 2, 8, Research.ENGINEERING,
             List.of(FIELD, DESERT, GRASS, TUNDRA, SNOW),
             List.of()
     );
 
-    //TODO... implement construction time
-
     private final int food;
     private final int gold;
     private final int production;
+    private final int constructionTime;
 
     private final Research prerequisite;
     private final ArrayList<Terrain> compatibleTerrains;
     private final ArrayList<Feature> compatibleFeatures;
 
-    Improvement(int food, int gold, int production, Research prerequisite,
+    Improvement(int food, int gold, int production, int constructionTime, Research prerequisite,
                 List<Terrain> compatibleTerrains, List<Feature> compatibleFeatures) {
         this.food = food;
         this.gold = gold;
         this.production = production;
+        this.constructionTime = constructionTime;
+
         this.prerequisite = prerequisite;
 
         this.compatibleTerrains = new ArrayList<>(compatibleTerrains);
@@ -88,6 +89,10 @@ public enum Improvement{
 
     public int getProduction() {
         return this.production;
+    }
+
+    public int getConstructionTime() {
+        return this.constructionTime;
     }
 
     public Research getPrerequisite() {
