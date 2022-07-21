b86f55418a68f62362e023f464c5f0c29bffd1d1
Mr.B <mrezabadri28@gmail.com>
2022-07-21 16:00:41 +0430

    city center and city names on tiles added

--- Client/src/main/java/graphics/statics/StaticImages.java
+++ Client/src/main/java/graphics/statics/StaticImages.java
@@ -7,4 +7,5 @@ public class StaticImages {
     public static ImagePattern FogOfWar = new ImagePattern(new Image("file:src/main/resources/images/map/FogOfWar.png"));
     public static ImagePattern FogOfWar2 = new ImagePattern(new Image("file:src/main/resources/images/map/FogOfWar2.png"));
     public static ImagePattern Ruin = new ImagePattern(new Image("file:src/main/resources/images/map/Ruin.png"));
+    public static ImagePattern City = new ImagePattern(new Image("file:src/main/resources/images/map/others/city.png"));
 }
