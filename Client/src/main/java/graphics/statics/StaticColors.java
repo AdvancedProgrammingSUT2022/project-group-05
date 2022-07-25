fd83e111bdb671661000abc3004e77942d27b839
Mr.B <mrezabadri28@gmail.com>
2022-07-25 20:42:48 +0430

    terranis added

--- Client/src/main/java/graphics/statics/StaticColors.java
+++ Client/src/main/java/graphics/statics/StaticColors.java
@@ -6,4 +6,5 @@ import javafx.scene.paint.Color;
 public class StaticColors {
     public static Color PopUpBackground = new Color(0.5, 0.5, 0.5, 0.6);
     public static Color PaneAnimation = Color.BLUE;
+    public static Color TileUnderColor = Color.WHITE;
 }
